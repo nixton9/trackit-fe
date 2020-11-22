3b51c0fd8d17a227b371b5b29fa7ec8bc55421d5
Eduardo <eduardojoaoaraujo@gmail.com>
2020-11-22 18:07:33 +0000

    Remove blue highlight when clicking on elements

--- src/styles/globalstyles.ts
+++ src/styles/globalstyles.ts
@@ -6,7 +6,10 @@ export const GlobalStyle = createGlobalStyle`
         padding: 0;
         margin: 0;
         box-sizing: border-box;
+        // Remove the blue highlight everytime we click on something on mobile
+        -webkit-tap-highlight-color: transparent;
     }
+
 
     html {
         font-size: 62.5%;
@@ -108,5 +111,6 @@ export const GlobalStyle = createGlobalStyle`
     .tag-tooltip .react-tooltip-lite {
         font-size: 1rem;
     }
+
 
 `
