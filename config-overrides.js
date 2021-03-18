633c9b13131ea65b49f7ff760db9d49d75212106
Eduardo <eduardojoaoaraujo@gmail.com>
2021-03-18 11:38:25 +0000

    Added back custom webpack config

---
+++ config-overrides.js
@@ -0,0 +1,21 @@
+const CompressionPlugin = require('compression-webpack-plugin')
+const BrotliPlugin = require('brotli-webpack-plugin')
+
+module.exports = function override(config, env) {
+  config.plugins.push(
+    new CompressionPlugin({
+      filename: '[path].gz[query]',
+      algorithm: 'gzip',
+      test: /\.(js|css|html|svg)$/,
+      threshold: 8192,
+      minRatio: 0.8
+    }),
+    new BrotliPlugin({
+      asset: '[path].br[query]',
+      test: /\.(js|css|html|svg)$/,
+      threshold: 10240,
+      minRatio: 0.8
+    })
+  )
+  return config
+}
